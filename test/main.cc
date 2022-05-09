a696ee6d49effd5134dd064f5fa4895c944ac941
X <70487315+XWasHere@users.noreply.github.com>
2022-05-09 12:35:28 -0400

    Add files via upload

--- test/main.cc
+++ test/main.cc
@@ -7,17 +7,17 @@ class PassTest : public Test {
     class Test : public ctest::Test {
         public:
         bool run() {
-            return true;
+            pass();
         }
     };
 
     bool run() {
         Test test = Test();
 
-        if (test.run()) {
-            return true;
+        if (test.exec()) {
+            pass();
         } else {
-            return false;
+            fail();
         }
     }
 };
@@ -27,18 +27,115 @@ class FailTest : public Test {
     class Test : public ctest::Test {
         public:
         bool run() {
-            return false;
+            fail();
         }
     };
 
     bool run() {
         Test test = Test();
 
-        if (test.run()) {
-            return false;
+        if (test.exec()) {
+            fail();
         } else {
-            return true;
+            pass();
         }
+    }
+};
+
+class QueueTest : public Test {
+    public:
+    class Test1 : public ctest::Test {
+        public:
+        bool run() {
+            pass();
+        }
+    };
+
+    class Test2 : public ctest::Test {
+        public:
+        bool run() {
+            fail();
+        }
+    };
+
+    bool run() {
+        TestController tc = TestController();
+        tc.logger->depth = logger->depth;
+
+        tc.queue<Test1>();
+        tc.queue<Test2>();
+
+        TestingResult* res = tc.run_all();
+
+        assert(res->failedc == 1, "1 test should have failed");
+        assert(res->passedc == 1, "1 test should have passed");
+        
+        pass();
+    }
+};
+
+class AssertTest : public Test {
+    public:
+    class PassTest : public ctest::Test {
+        public:
+        class Test : public ctest::Test {
+            public:
+            bool run() {
+                assert(true, "if youre seeing this the thing broke");
+                pass();
+            }
+        };
+
+        bool run() {
+            TestController tc = TestController();
+            tc.logger->depth = logger->depth;
+
+            tc.queue<Test>();
+
+            TestingResult* res = tc.run_all();
+
+            assert(res->passedc == 1, "the test should have passed");
+
+            pass();
+        }
+    };
+    
+    class FailTest : public ctest::Test {
+        public:
+        class Test : public ctest::Test {
+            public:
+            bool run() {
+                assert(false, "if youre seeing this the thing worked");
+                pass();
+            }
+        };
+        
+        bool run() {
+            TestController tc = TestController();
+            tc.logger->depth = logger->depth;
+
+            tc.queue<Test>();
+
+            TestingResult* res = tc.run_all();
+
+            assert(res->failedc == 1, "the test should have failed");
+            
+            pass();
+        }
+    };
+
+    bool run() {
+        TestController tc = TestController();
+        tc.logger->depth = logger->depth;
+
+        tc.queue<PassTest>();
+        tc.queue<FailTest>();
+
+        TestingResult* res = tc.run_all();
+
+        assert(res->failedc == 0);
+
+        pass();
     }
 };
 
@@ -47,6 +144,8 @@ int main() {
 
     tc.queue<PassTest>();
     tc.queue<FailTest>();
+    tc.queue<QueueTest>();
+    tc.queue<AssertTest>();
 
     tc.run_all();
 }
