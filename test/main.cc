1c0604b7831267e969c1884cae760576b5c479bf
X <70487315+XWasHere@users.noreply.github.com>
2022-05-11 12:05:15 -0400

    Add files via upload

--- test/main.cc
+++ test/main.cc
@@ -139,6 +139,128 @@ class AssertTest : public Test {
     }
 };
 
+class ExpectTest : public Test {
+    public:
+    class ExpectException : public ctest::Test {
+        public:
+        class Exception1 : public std::exception {};
+        class Exception2 : public std::exception {};
+
+        class Fails : public ctest::Test {
+            public:
+            bool run() {
+                expect()->exception<Exception1>()->in([]{
+                    
+                });
+
+                pass();
+            }
+        };
+
+        class Passes : public ctest::Test {
+            public:
+            bool run() {
+                expect()->exception<Exception1>()->in([]{
+                    throw Exception1();
+                });
+
+                pass();
+            }
+        };
+
+        class WouldCrash : public ctest::Test {
+            public:
+            bool run() {
+                try {
+                    expect()->exception<Exception1>()->in([]{
+                        throw Exception2();
+                    });
+                } catch (Exception2& e) {
+                    logger->cprintf(logger->info, "\"fake crash\"\n");
+                }
+
+                pass();
+            }
+        };
+
+        class Multi11 : public ctest::Test {
+            public:
+            bool run() {
+                expect()->exception<Exception1>()->exception<Exception2>()->in([]{
+                    throw Exception1();
+                });
+
+                pass();
+            }
+        };
+
+        class Multi12 : public ctest::Test {
+            public:
+            bool run() {
+                expect()->exception<Exception1>()->exception<Exception2>()->in([]{
+                    throw Exception2();
+                });
+
+                pass();
+            }
+        };
+
+        class Multi21 : public ctest::Test {
+            public:
+            bool run() {
+                expect()->exception<Exception2>()->exception<Exception1>()->in([]{
+                    throw Exception1();
+                });
+
+                pass();
+            }
+        };
+
+        class Multi22 : public ctest::Test {
+            public:
+            bool run() {
+                expect()->exception<Exception2>()->exception<Exception1>()->in([]{
+                    throw Exception2();
+                });
+
+                pass();
+            }
+        };
+
+        bool run() {
+            TestController tc = TestController();
+            tc.logger->depth = logger->depth;
+
+            tc.queue<Fails>();
+            tc.queue<Passes>();
+            tc.queue<WouldCrash>();
+            tc.queue<Multi11>();
+            tc.queue<Multi12>();
+            tc.queue<Multi21>();
+            tc.queue<Multi22>();
+
+            TestingResult* res = tc.run_all();
+
+            assert(res->failedc == 1);
+
+            pass();    
+        }
+    };
+
+    bool run() {
+        TestController tc = TestController();
+        tc.logger->depth = logger->depth;
+
+        tc.queue<ExpectException>();
+
+        TestingResult* res = tc.run_all();
+
+        assert(res->failedc == 0);
+
+        pass();
+    }
+};
+
 int main() {
     TestController tc = TestController();
 
@@ -146,6 +268,7 @@ int main() {
     tc.queue<FailTest>();
     tc.queue<QueueTest>();
     tc.queue<AssertTest>();
+    tc.queue<ExpectTest>();
 
     tc.run_all();
 }
